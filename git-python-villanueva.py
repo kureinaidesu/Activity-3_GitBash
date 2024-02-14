aac61a2456e0e535a9fb8840eed871f7d35a43bd
VillanuevaDhanAubrey <dhanaubrey@gmail.com>
2024-02-14 21:05:35 +0800

    My Personal Info Added

---
+++ git-python-villanueva.py
@@ -0,0 +1,16 @@
+# git-python-villanueva.py
+
+# Display full name
+print("Dhanaubrey Villanueva")
+
+# Display address
+print("Caganhao Boac, Marinduque")
+
+# Display complete birthday
+print("March 22, 2002")
+
+print("Marinduque State College")       
+print("BSI/T")              
+print("2nd year")                          
+print("2A")                            
+
